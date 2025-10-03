f1fc3f42c231e09f7c3f09b7af52ee49fdc71cef
Mikias7 <mikias7t@gmail.com>
2025-10-03 17:57:47 -0500

    Polished the Nav component

--- portfolio/src/components/Nav.jsx
+++ portfolio/src/components/Nav.jsx
@@ -1,15 +1,35 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Nav(){
 
     return (
-        <div className="">
-            <div>
-                <nav className="">
-                    <div><Link to="/">Home</Link></div>
-                    <div><Link to="/experience">Experience</Link></div>
-                    <div><Link to="/projects">projects</Link></div>
-                    <div><Link to="/contact">Contact</Link></div>
+        <div className="pt-10 w-screen">
+            <div className="pl-12 pr-12 ">
+                <nav className="flex justify-center gap-20 bg-[rgba(58,59,61,0.35)] border border-red-500 ring ring-red-700 ring-offset-2 shadow-lg shadow-red-500/50 opacity-75 rounded-md">
+                    <div className="hover:text-red-700/100 hover:underline decoration-solid">
+                        <NavLink to="/" className={({ isActive}) => isActive
+                            ? "text-red-700 font-bold underline decoration-solid"
+                            : ""}>Home</NavLink>
+                    </div>
+
+                    <div className="hover:text-red-700/100 hover:underline decoration-solid">
+                        <NavLink to="/experience" className={({ isActive}) => isActive
+                            ? "text-red-700 font-bold underline decoration-solid"
+                            : ""}>Experience</NavLink>
+                    </div>
+
+                    <div className="hover:text-red-700/100 hover:underline decoration-solid">
+                        <NavLink to="/projects" className={({ isActive}) => isActive
+                            ? "text-red-700 font-bold underline decoration-solid"
+                            : ""}>projects</NavLink>
+                    </div>
+
+                    <div className="hover:text-red-700/100 hover:underline decoration-solid">
+                        <NavLink to="/contact" className={({ isActive}) => isActive
+                            ? "text-red-700 font-bold underline decoration-solid"
+                            : ""}>Contact</NavLink>
+                    </div>
+
                 </nav>
             </div>
         </div>
